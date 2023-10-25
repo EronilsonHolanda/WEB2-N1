bd2fce44cec49c1ef6cea7c7a3b4ab85210c8dfe
EronilsonHolanda <127308928+EronilsonHolanda@users.noreply.github.com>
2023-10-25 13:35:51 -0300

    Desativação do CSRF.java

--- evento/src/main/java/com/evento/evento/config/SecurityConfig.java
+++ evento/src/main/java/com/evento/evento/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
         .build();
         return new InMemoryUserDetailsManager(admin);
     }
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable);
